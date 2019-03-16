80a4eae506291c284dfc3d723c80c6070b492f88
Gabriel Ungier <gabe@Gabriels-MBP.attlocal.net>
2019-03-15 23:37:21 -0400

    part of onClick and adjusted a lil bit of HTML

--- assets/javascript/gabe.js
+++ assets/javascript/gabe.js
@@ -15,10 +15,27 @@ $(document).ready(function () {
 
             for (var x=0; x<data.categories.length; x++) {
                 var i = data.categories[x];
-                debugger;
                 var zomatoData = "<option value=" + i.categories.id + ">" + i.categories.name + "</option>";
-                $(zomatoData).appendTo('.zomato-result');
+                $(zomatoData).appendTo('#foodOption');
             }
+            $("#foodOption").on('click', function(){
+                event.preventDefault();
+                var queryURL = '"https://developers.zomato.com/api/v2.1/searchCity"' + zomatoData;
+                $.ajax({
+                    url:  queryURL,
+                    method: 'GET',
+                    dataType: 'JSON',
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader('Accept', 'application/json');
+                        xhr.setRequestHeader('user-key', 'c81fcbb8bdb56196b1c05bb81e9c85d6');
+                    }
+                }).then(function (select) {
+                    console.log(select);
+        
+        
+                })
         });
     });
+
+    })
 
