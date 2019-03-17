2806447624f2745188b4b5afc343881c2e7d3912
Gabriel Ungier <gabe@Gabriels-MBP.attlocal.net>
2019-03-16 21:12:17 -0400

    OMDB call should work

--- assets/javascript/gabe.js
+++ assets/javascript/gabe.js
@@ -3,19 +3,22 @@ var genresComedy = ['Superbad', 'Big Lebowski', 'This is Spinal Tap', 'Zoolander
 var genresDrama = ['Forrest Gump', 'A Beautiful Mind', 'Lady Bird', "What's Eating Gilbert Grape", 'Slumdog Millionaire'];
 var genresRomCom = ['When Harry Met Sally', '10 Things I Hate About You', 'The Wedding Singer', 'Forgetting Sarah Marshall', 'Reality Bites'];
 var genresSyFy = ['Donnie Darko', 'Dawn of the Planet of the Apes', 'Ex Machina', 'Edge of Tomorrow'];
-var movie = [];
+var movie = [genresSyFy, genresRomCom, genresDrama, genresComedy];
 
 var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=a86b8a6d";
 
-for (var i = 0; i < movie.lenghth; i++){
-    var movie = genres.categories[i];
-}
+
+
 
 $.ajax({
     url: queryURL,
     method: "get"
 }).then(function(response){
-console.log(response)
+    console.log(response)
+    for (var i = 0; i < movie.lenghth; i++){
+        var movie = genres.categories[i];
+   console.log(movies); 
+    }
 });
 
 
