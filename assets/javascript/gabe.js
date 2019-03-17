b30260644c92bda0a650b214df9634b27d5be5b9
Gabriel Ungier <gabe@Gabriels-MBP.attlocal.net>
2019-03-17 17:54:43 -0400

    script tag not linking appropriately?

--- assets/javascript/gabe.js
+++ assets/javascript/gabe.js
@@ -3,21 +3,24 @@ var genresComedy = ['Superbad', 'Big Lebowski', 'This is Spinal Tap', 'Zoolander
 var genresDrama = ['Forrest Gump', 'A Beautiful Mind', 'Lady Bird', "What's Eating Gilbert Grape", 'Slumdog Millionaire'];
 var genresRomCom = ['When Harry Met Sally', '10 Things I Hate About You', 'The Wedding Singer', 'Forgetting Sarah Marshall', 'Reality Bites'];
 var genresSyFy = ['Donnie Darko', 'Dawn of the Planet of the Apes', 'Ex Machina', 'Edge of Tomorrow'];
-var movie = [genresSyFy, genresRomCom, genresDrama, genresComedy];
+var movies = [genresSyFy, genresRomCom, genresDrama, genresComedy];
 
-var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=a86b8a6d";
-
-
+var queryURL = "https://www.omdbapi.com/?t=" + movies + "&y=&plot=short&apikey=a86b8a6d";
 
 
+
+$(document).ready()
 $.ajax({
     url: queryURL,
     method: "get"
 }).then(function(response){
     console.log(response)
-    for (var i = 0; i < movie.lenghth; i++){
-        var movie = genres.categories[i];
-   console.log(movies); 
+
+    for (var x=0; x<movies.length; x++) {
+        var i = movies[x];
+        var movieData = "<option value=" + i.movies + "</option>";
+        $(movieData).appendTo('#movieChoices');
+   console.log("should show list of movies: " + movieData); 
     }
 });
 
